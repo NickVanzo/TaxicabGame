b9afeb624efafa5fbe3ff77781fbe6a87eaa63b1
Nick <nicolo.vanzo@edu.unito.it>
2020-12-07 10:04:54 +0100

    Typo risolti

--- taxi.c
+++ taxi.c
@@ -8,31 +8,31 @@ int main(int argc, char * argv[]){
 
 		/*Apertura coda di messaggi*/	
 		queue_key = ftok("ipcKey.key", 1);
-		if(queueKey == -1){
+		if(queue_key == -1){
         	printf("Error retriving message queue key!\n");
         	exit(EXIT_FAILURE);
     	}	
 
 		queue_id  = msgget(queue_key, 0);
-		if(queueId == -1){
+		if(queue_id == -1){
         	printf("Error retriving queue id!\n");
         	exit(EXIT_FAILURE);
     	}
 
 		/*Attach alla memoria condivisa*/
 		shm_Key = ftok("ipcKey.key", 2);
-		if(shmKey == -1){
+		if(shm_Key == -1){
         	printf("Error retriving shared memory key!\n");
         	exit(EXIT_FAILURE);
     	}
 
-		shm_id = shmget(shm_Key, sizeof(struct grigliaCitta, 0));
-    	if(shmId == -1) {
+		shm_id = shmget(shm_Key, sizeof(struct grigliaCitta), 0);
+    	if(shm_id == -1) {
         	printf("Error retriving shared memory ID!\n");
         	exit(EXIT_FAILURE);
     	}
 
-    	mappa = shmat(shmId, NULL, 0);
+    	mappa = shmat(shm_id, NULL, 0);
     	if(mappa == (struct grigliaCitta *)(-1)){
         	printf("Error attaching memory segment!\n");
         	exit(EXIT_FAILURE);
